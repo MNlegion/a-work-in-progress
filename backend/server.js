d9ed1c6fefc29f1ccc29788d44646796a6e40726
MNlegion <rkreuser30@gmail.com>
2024-02-08 22:29:48 -0500

    develop branch initialize

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+// Create Express app
 const app = express();
 
+// A simple route
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
 });
 
+// Start the Express server
 app.listen(5000, () => {
   console.log('Server listening on http://localhost:5000');
 });
