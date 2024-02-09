0024ef8f1c6848d20fe0ff3125ff2a84b196e7c8
MNlegion <rkreuser30@gmail.com>
2024-02-08 22:27:43 -0500

    initial server setup

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+app.listen(5000, () => {
+  console.log('Server listening on http://localhost:5000');
+});
