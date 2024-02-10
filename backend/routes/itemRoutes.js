8f11247b8898227ae50e62e200a6d08259fa2ff7
MNlegion <rkreuser30@gmail.com>
2024-02-10 12:17:23 -0500

    reset of itemroutes

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const { getItems, getItemById , createItem, updateItem, deleteItem } = require('../controllers/itemController');
 
-
-// Get all items and create a new item
-router.route('/').get(getItems).post(createItem);
-
-// Get single item by ID, update an item, and delete an item
-router.route('/:id').get(getItemById).put(updateItem).delete(deleteItem);
-
-
-// Export the router
-module.exports = router;
