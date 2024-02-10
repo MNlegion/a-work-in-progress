b29946c22a938af1d8574023fae1572c048595d5
MNlegion <rkreuser30@gmail.com>
2024-02-10 12:51:42 -0500

    server and item route setup

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from server!' });
+});
+
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Create item!' });
+});
+
+router.put('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from server!' });
+});
+
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from server!' });
+});
+
+
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
-});
+// import routes
+app.use('/api/items', require('./routes/itemRoutes'));
 
 app.listen(5000, () => {
-  console.log('Server listening on http://localhost:5000');
+  console.log(`Server listening on ${port}`);
 });
