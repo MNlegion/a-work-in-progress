c16ffd676ac1e83f2a986a2c98b7763366d21dcf
MNlegion <rkreuser30@gmail.com>
2024-02-09 21:43:11 -0500

    restart on itemRoutes server

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const {errorHandler} = require('./middleware/errorMiddleware');
-const port = process.env.PORT || 5000;
 
-// Create Express app
+
 const app = express();
 
-// middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
 
-// Import Routes
-app.use('/api/items', require('./routes/itemRoutes'));
-
-// Start the Express server
 app.listen(5000, () => {
   console.log('Server listening on http://localhost:5000');
 });
