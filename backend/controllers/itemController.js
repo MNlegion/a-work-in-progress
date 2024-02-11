26f4fd95790d9a02e3da6983315f9e80aed1c506
MNlegion <rkreuser30@gmail.com>
2024-02-10 19:59:09 -0500

    asyncHandler setup

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,51 +1,49 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc: Get all items
 // @route: GET /api/items
 // @access: Public
-const getItems = (req, res) => {
-    res.status(200).json({ message: 'Get all items' });
-}
+const getItems = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get all items" });
+});
 
 // @desc: Get single item
 // @route: GET /api/items/:id
 // @access: Public
-const getItem = (req, res) => {
-    res.status(200).json({ message: `Get Item ${req.params.id}` });
-}
+const getItem = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Get Item ${req.params.id}` });
+});
 
 // @desc: Create item
 // @route: POST /api/items
 // @access: Private
-const createItem = (req, res) => {
-    res.status(200).json({ message: 'Create item!' });
-}
+const createItem = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Create item!" });
+});
 
 // @desc: Update item
 // @route: PUT /api/items/:id
 // @access: Private
-const updateItem = (req, res) => {
-    if(!req.body.text) {
-        res.status(400)
-        throw new Error('Please provide data');
-    }
+const updateItem = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please provide data");
+  }
 
-    console.log(req.body);
-
-    res.status(200).json({ message: `Update Item ${req.body}` });
-}
+  res.status(200).json({ message: `Update Item ${req.body.text}` });
+});
 
 // @desc: Delete item
 // @route: DELETE /api/items/:id
 // @access: Private
-const deleteItem = (req, res) => {
-    res.status(200).json({ message: `Delete Item ${req.params.id}` });
-}
-
-
+const deleteItem = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete Item ${req.params.id}` });
+});
 
 module.exports = {
-    getItems,
-    getItem,
-    createItem,
-    updateItem,
-    deleteItem
-}
+  getItems,
+  getItem,
+  createItem,
+  updateItem,
+  deleteItem,
+};
