5db9ce32acec79d9cc9b23919f2ec488258130de
MNlegion <rkreuser30@gmail.com>
2024-02-10 19:26:30 -0500

    condensed routes in itemRoutes

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -2,16 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getItems, getItem, updateItem, createItem, deleteItem } = require('../controllers/itemController');
 
-// Routes
-router.get('/', getItems);
-
-router.get('/:id', getItem);
-
-router.post('/', createItem);
-
-router.put('/:id', updateItem);
-
-router.delete('/:id', deleteItem);
+// condensed routes
+router.route('/').get(getItems).post(createItem);
+router.route('/:id').get(getItem).put(updateItem).delete(deleteItem);
 
 
 module.exports = router;
