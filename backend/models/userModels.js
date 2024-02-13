ef8635e9f0af7365514d00e3dfa110ef5e303508
MNlegion <rkreuser30@gmail.com>
2024-02-12 19:32:35 -0500

    initial user model setup

---
+++ backend/models/userModels.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+        type: String,
+        required: [true, 'Please enter your username'],
+        },
+        email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+        },
+        password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        },
+        passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        },
+        isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+    );
+
+
+module.exports = mongoose.model('User', userSchema);
