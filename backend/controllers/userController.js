cc7e1c1e08c92b64873f629866151f6d5d37917c
MNlegion <rkreuser30@gmail.com>
2024-02-12 20:57:28 -0500

    user controller function setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,27 @@
-const registerUser = (req, res) => {
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+// @desc: Register a new user
+// @route: POST /api/users/
+// @access: Public
+const registerUser = asyncHandler(async (req, res) => {
   res.json({ message: "User Registered" });
-};
+});
 
-const loginUser = (req, res) => {
+// @desc: Login user
+// @route: POST /api/users/login
+// @access: Public
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "User Login Successful" });
-};
+});
 
-const getUserProfile = (req, res) => {
+// @desc: Get user profile
+// @route: GET /api/users/profile
+// @access: Private
+const getUserProfile = asyncHandler(async (req, res) => {
   res.json({ message: "User Profile Loaded Successfully" });
-};
+});
 
 module.exports = { registerUser, loginUser, getUserProfile };
