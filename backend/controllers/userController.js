9db51358c1fc6a934f153f4d4b73228cb383d606
MNlegion <rkreuser30@gmail.com>
2024-02-13 21:06:49 -0500

    bcrypt password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,46 @@ const User = require("../models/userModel");
 // @route: POST /api/users/
 // @access: Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "User Registered" });
+    const { name, email, password } = req.body;
+    
+    if(!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please provide all fields");
+    }
+
+    // check if user already exists
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: "30d",
+            }),
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+
+    res.json({ message: "User Registered Successfully" });
 });
 
 // @desc: Login user
