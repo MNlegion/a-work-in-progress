bd2cfb6dd0b7b0f9064d2708152d2e1a0989689c
MNlegion <rkreuser30@gmail.com>
2024-02-14 21:23:44 -0500

    user login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route: POST /api/users/login
 // @access: Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "User Login Successful" });
+  const { email, password } = req.body;
+
+  //   Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc: Get user profile
