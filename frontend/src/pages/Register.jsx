d2938f2a94cfa99e211f8d97f5bbeca8ec268652
MNlegion <rkreuser30@gmail.com>
2024-02-18 15:38:31 -0500

    login form setup

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,13 @@ function Register() {
 
   const { name, email, password, password2 } = formData;
 
-  const onChange = (e) => {};
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
   };
