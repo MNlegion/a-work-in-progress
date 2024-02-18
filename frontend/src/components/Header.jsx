00bba91043760a8ed7163edde9e18733769403ab
MNlegion <rkreuser30@gmail.com>
2024-02-18 17:05:37 -0500

    login function slice and service setup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,19 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser, FaUserPlus } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <header className="header">
       <div className="logo">
@@ -9,25 +21,36 @@ function Header() {
           <h1>A Work In Progress</h1>
         </Link>
       </div>
-        <nav>
-            <ul>
-            <li>
-                <Link to="/dashboard">
-                <FaUser /> Dashboard
-                </Link>
-            </li>
-            <li>
+      <nav>
+        <ul>
+          {user ? (
+            <><li>
+            <Link to="/dashboard">
+              <FaUser /> Dashboard
+            </Link>
+          </li>
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+          </>
+          ) : (
+            <>
+              <li>
                 <Link to="/login">
-                <FaSignInAlt /> Login
+                  <FaSignInAlt /> Login
                 </Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to="/register">
-                <FaUserPlus /> Register
+                  <FaUserPlus /> Register
                 </Link>
-            </li>
-            </ul>
-        </nav>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     </header>
   );
 }
